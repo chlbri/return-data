7d8895d34764a47e1f9fdeaed49ee426ba532228
chlbri <chlbri.blac@gmail.com>
2021-10-27 08:17:48 +0000

    format

--- dist/index.js
+++ dist/index.js
@@ -76,124 +76,46 @@ class ReturnData {
         }
         return client(data.status, data.message);
     }
-    successMap({ information, client, permission, redirect, server, success, timeout, }) {
-        const data = this.data;
-        if ((0, functions_1.isInformation)(data)) {
-            if (!information)
-                return (0, exports.error)();
-            return information(data.status, data.payload, data.message);
-        }
-        if ((0, functions_1.isPermission)(data)) {
-            if (!permission)
-                return (0, exports.error)();
-            return permission(data.status, data.payload, data.notPermitteds);
-        }
-        if ((0, functions_1.isRedirect)(data)) {
-            if (!redirect)
-                return (0, exports.error)();
-            return redirect(data.status, data.payload, data.message);
-        }
-        if ((0, functions_1.isServer)(data)) {
-            if (!server)
-                return (0, exports.error)();
-            return server(data.status, data.message);
-        }
-        if ((0, functions_1.isSuccess)(data)) {
-            return success(data.status, data.payload);
-        }
-        if ((0, functions_1.isTimeout)(data)) {
-            if (!timeout)
-                return (0, exports.error)();
-            return timeout(data.status);
-        }
-        if (!client)
-            return (0, exports.error)();
-        return client(data.status, data.message);
+    successMap(cases) {
+        // #region Cases
+        var _a, _b, _c, _d, _e, _f;
+        const information = (_a = cases.information) !== null && _a !== void 0 ? _a : exports.error;
+        const permission = (_b = cases.permission) !== null && _b !== void 0 ? _b : exports.error;
+        const redirect = (_c = cases.redirect) !== null && _c !== void 0 ? _c : exports.error;
+        const server = (_d = cases.server) !== null && _d !== void 0 ? _d : exports.error;
+        const success = cases.success;
+        const timeout = (_e = cases.timeout) !== null && _e !== void 0 ? _e : exports.error;
+        const client = (_f = cases.client) !== null && _f !== void 0 ? _f : exports.error;
+        // #endregion
+        return this.map({
+            client,
+            information,
+            permission,
+            redirect,
+            server,
+            success,
+            timeout,
+        });
     }
     maybeMap(cases) {
+        // #region Cases
         var _a, _b, _c, _d, _e, _f, _g;
-        const data = this.data;
-        if ((0, functions_1.isInformation)(data)) {
-            const information = (_a = cases.information) !== null && _a !== void 0 ? _a : cases.else;
-            return this.map({
-                information,
-                client: cases.else,
-                permission: cases.else,
-                redirect: cases.else,
-                success: cases.else,
-                server: cases.else,
-                timeout: cases.else,
-            });
-        }
-        if ((0, functions_1.isPermission)(data)) {
-            const permission = (_b = cases.permission) !== null && _b !== void 0 ? _b : cases.else;
-            return this.map({
-                information: cases.else,
-                client: cases.else,
-                permission,
-                redirect: cases.else,
-                success: cases.else,
-                server: cases.else,
-                timeout: cases.else,
-            });
-        }
-        if ((0, functions_1.isRedirect)(data)) {
-            const redirect = (_c = cases.redirect) !== null && _c !== void 0 ? _c : cases.else;
-            return this.map({
-                information: cases.else,
-                client: cases.else,
-                permission: cases.else,
-                redirect,
-                success: cases.else,
-                server: cases.else,
-                timeout: cases.else,
-            });
-        }
-        if ((0, functions_1.isServer)(data)) {
-            const server = (_d = cases.server) !== null && _d !== void 0 ? _d : cases.else;
-            return this.map({
-                information: cases.else,
-                client: cases.else,
-                permission: cases.else,
-                redirect: cases.else,
-                success: cases.else,
-                server,
-                timeout: cases.else,
-            });
-        }
-        if ((0, functions_1.isSuccess)(data)) {
-            const success = (_e = cases.success) !== null && _e !== void 0 ? _e : cases.else;
-            return this.map({
-                information: cases.else,
-                client: cases.else,
-                permission: cases.else,
-                redirect: cases.else,
-                server: cases.else,
-                success,
-                timeout: cases.else,
-            });
-        }
-        if ((0, functions_1.isTimeout)(data)) {
-            const timeout = (_f = cases.timeout) !== null && _f !== void 0 ? _f : cases.else;
-            return this.map({
-                information: cases.else,
-                client: cases.else,
-                permission: cases.else,
-                redirect: cases.else,
-                success: cases.else,
-                timeout,
-                server: cases.else,
-            });
-        }
-        const client = (_g = cases.client) !== null && _g !== void 0 ? _g : cases.else;
+        const client = (_a = cases.client) !== null && _a !== void 0 ? _a : cases.else;
+        const information = (_b = cases.information) !== null && _b !== void 0 ? _b : cases.else;
+        const permission = (_c = cases.permission) !== null && _c !== void 0 ? _c : cases.else;
+        const redirect = (_d = cases.redirect) !== null && _d !== void 0 ? _d : cases.else;
+        const server = (_e = cases.server) !== null && _e !== void 0 ? _e : cases.else;
+        const success = (_f = cases.success) !== null && _f !== void 0 ? _f : cases.else;
+        const timeout = (_g = cases.timeout) !== null && _g !== void 0 ? _g : cases.else;
+        // #endregion
         return this.map({
-            information: cases.else,
             client,
-            permission: cases.else,
-            redirect: cases.else,
-            success: cases.else,
-            timeout: cases.else,
-            server: cases.else,
+            information,
+            permission,
+            redirect,
+            server,
+            success,
+            timeout,
         });
     }
 }
